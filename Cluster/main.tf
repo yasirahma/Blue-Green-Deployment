db08d7f504d5ed59093b9f056a104de7d2592952
YASIR AHAMAD <130695697+yasirahma@users.noreply.github.com>
2026-04-24 23:31:57 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -97,7 +97,7 @@ resource "aws_eks_cluster" "devops" {
 }
 
 resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devops.name
+  cluster_name    = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
   node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
   subnet_ids      = aws_subnet.devopsshack_subnet[*].id
